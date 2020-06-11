f902c286848cba5dc65d25b420f4c91076d0270f
Marco <Prosperi>
2020-06-11 11:04:40 +0200

    carico tutto a documento pronto

--- js/script.js
+++ js/script.js
@@ -1,33 +1,37 @@
-/*
-quando il puntatore del mouse entra nel menu faccio aprire
-il dropdown e la freccia della lingua va verso l'alto
-*/
-$('.ms_dropdown_link').mouseenter(
-  function () {
-    $(this).children('.ms_dropmenu').removeClass('ms_hidden')
-    $('.ms_lang i').toggleClass('fa-angle-up')
-    $('.ms_lang i').toggleClass('fa-angle-down')
-  }
-)
-/*
-quando il puntatore del mouse esce dal menu faccio chiudere
-il dropdown e la freccia della lingua va verso il basso
-*/
-$('.ms_dropdown_link').mouseleave(
-  function () {
-    $(this).children('.ms_dropmenu').addClass('ms_hidden')
-    $('.ms_lang i').toggleClass('fa-angle-up')
-    $('.ms_lang i').toggleClass('fa-angle-down')
-  }
-)
-/*
-al click sull'elemento del menu faccio chiudere/aprire il dropdown
-e andare in basso/in alto la freccia della lingua
-*/
-$('.ms_dropdown_link').click(
-  function () {
-    $(this).children('.ms_dropmenu').toggleClass('ms_hidden')
-    $('.ms_lang i').toggleClass('fa-angle-up')
-    $('.ms_lang i').toggleClass('fa-angle-down')
+$(document).ready(
+  function() {
+    /*
+    quando il puntatore del mouse entra nel menu faccio aprire
+    il dropdown e la freccia della lingua va verso l'alto
+    */
+    $('.ms_dropdown_link').mouseenter(
+      function () {
+        $(this).children('.ms_dropmenu').removeClass('ms_hidden')
+        $('.ms_lang i').toggleClass('fa-angle-up')
+        $('.ms_lang i').toggleClass('fa-angle-down')
+      }
+    )
+    /*
+    quando il puntatore del mouse esce dal menu faccio chiudere
+    il dropdown e la freccia della lingua va verso il basso
+    */
+    $('.ms_dropdown_link').mouseleave(
+      function () {
+        $(this).children('.ms_dropmenu').addClass('ms_hidden')
+        $('.ms_lang i').toggleClass('fa-angle-up')
+        $('.ms_lang i').toggleClass('fa-angle-down')
+      }
+    )
+    /*
+    al click sull'elemento del menu faccio chiudere/aprire il dropdown
+    e andare in basso/in alto la freccia della lingua
+    */
+    $('.ms_dropdown_link').click(
+      function () {
+        $(this).children('.ms_dropmenu').toggleClass('ms_hidden')
+        $('.ms_lang i').toggleClass('fa-angle-up')
+        $('.ms_lang i').toggleClass('fa-angle-down')
+      }
+    )
   }
 )
